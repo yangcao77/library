1713b4172f1fdbb608b2f162bda3c2f5e64d1c70
Stephanie <yangcao@redhat.com>
2020-11-23 14:02:13 -0500

    save main.go changes to be able to parse from url
    Signed-off-by: Stephanie <yangcao@redhat.com>

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"reflect"
 
 	devfilepkg "github.com/devfile/library/pkg/devfile"
@@ -10,10 +11,19 @@ import (
 )
 
 func main() {
-	devfile, err := ParseDevfile("devfile.yaml")
+	var devfile parser.DevfileObj
+	var err error
+	if len(os.Args) > 1 {
+		devfile, err = devfilepkg.ParseFromURLAndValidate(os.Args[1])
+		fmt.Println("parsing devfile from " + os.Args[1])
+	} else {
+		devfile, err = ParseDevfile("devfile.yaml")
+		fmt.Println("parsing devfile from " + devfile.Ctx.GetAbsPath())
+	}
 	if err != nil {
 		fmt.Println(err)
 	} else {
+
 		devdata := devfile.Data
 		if (reflect.TypeOf(devdata) == reflect.TypeOf(&v2.DevfileV2{})) {
 			d := devdata.(*v2.DevfileV2)
@@ -27,9 +37,10 @@ func main() {
 		}
 
 		for _, command := range devfile.Data.GetCommands() {
-			if command.Exec != nil {
-				fmt.Println(command.Exec.Group.Kind)
-			}
+			// if command.Exec != nil {
+			// 	fmt.Println(command.Exec.Group.Kind)
+			// }
+			fmt.Println(command.Id)
 		}
 	}
 
