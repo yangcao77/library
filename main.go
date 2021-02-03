ada1f260adea41b1e83066e669589a014b459336
Stephanie <yangcao@redhat.com>
2021-02-03 17:10:34 -0500

    test

--- main.go
+++ main.go
@@ -45,65 +45,65 @@ func main() {
 			fmt.Printf("%s\n", component.Name)
 		}
 
-		fmt.Printf("All Exec commands: \n")
-		commands, e := devfile.Data.GetCommands(common.DevfileOptions{})
-		if e != nil {
-			fmt.Printf("err: %v\n", err)
-		}
-		for _, command := range commands {
-			if command.Exec != nil {
-				fmt.Printf("command %s is with kind: %s", command.Id, command.Exec.Group.Kind)
-				fmt.Printf("workingDir is: %s\n", command.Exec.WorkingDir)
-			}
-		}
-
-		fmt.Println("=========================================================")
-
-		compOptions := common.DevfileOptions{
-			Filter: map[string]interface{}{
-				"tool": "console-import",
-				"import": map[string]interface{}{
-					"strategy": "Dockerfile",
-				},
-			},
-		}
-
-		components, e = devfile.Data.GetComponents(compOptions)
-		if e != nil {
-			fmt.Printf("err: %v\n", err)
-		}
-		fmt.Printf("Container components applied filter: \n")
-		for _, component := range components {
-			if component.Container != nil {
-				fmt.Printf("%s\n", component.Name)
-			}
-		}
-
-		cmdOptions := common.DevfileOptions{
-			Filter: map[string]interface{}{
-				"tool": "odo",
-			},
-		}
-
-		fmt.Printf("Exec commands applied filter: \n")
-		commands, e = devfile.Data.GetCommands(cmdOptions)
-		if e != nil {
-			fmt.Printf("err: %v\n", err)
-		}
-		for _, command := range commands {
-			if command.Exec != nil {
-				fmt.Printf("command %s is with kind: %s", command.Id, command.Exec.Group.Kind)
-				fmt.Printf("workingDir is: %s\n", command.Exec.WorkingDir)
-			}
-		}
-
-		var err error
-		metadataAttr := devfile.Data.GetMetadata().Attributes
-		dockerfilePath := metadataAttr.GetString("alpha.build-dockerfile", &err)
-		if err != nil {
-			fmt.Printf("err: %v\n", err)
-		}
-		fmt.Printf("dockerfilePath: %s\n", dockerfilePath)
+		//fmt.Printf("All Exec commands: \n")
+		//commands, e := devfile.Data.GetCommands(common.DevfileOptions{})
+		//if e != nil {
+		//	fmt.Printf("err: %v\n", err)
+		//}
+		//for _, command := range commands {
+		//	if command.Exec != nil {
+		//		fmt.Printf("command %s is with kind: %s", command.Id, command.Exec.Group.Kind)
+		//		fmt.Printf("workingDir is: %s\n", command.Exec.WorkingDir)
+		//	}
+		//}
+		//
+		//fmt.Println("=========================================================")
+		//
+		//compOptions := common.DevfileOptions{
+		//	Filter: map[string]interface{}{
+		//		"tool": "console-import",
+		//		"import": map[string]interface{}{
+		//			"strategy": "Dockerfile",
+		//		},
+		//	},
+		//}
+		//
+		//components, e = devfile.Data.GetComponents(compOptions)
+		//if e != nil {
+		//	fmt.Printf("err: %v\n", err)
+		//}
+		//fmt.Printf("Container components applied filter: \n")
+		//for _, component := range components {
+		//	if component.Container != nil {
+		//		fmt.Printf("%s\n", component.Name)
+		//	}
+		//}
+		//
+		//cmdOptions := common.DevfileOptions{
+		//	Filter: map[string]interface{}{
+		//		"tool": "odo",
+		//	},
+		//}
+		//
+		//fmt.Printf("Exec commands applied filter: \n")
+		//commands, e = devfile.Data.GetCommands(cmdOptions)
+		//if e != nil {
+		//	fmt.Printf("err: %v\n", err)
+		//}
+		//for _, command := range commands {
+		//	if command.Exec != nil {
+		//		fmt.Printf("command %s is with kind: %s", command.Id, command.Exec.Group.Kind)
+		//		fmt.Printf("workingDir is: %s\n", command.Exec.WorkingDir)
+		//	}
+		//}
+		//
+		//var err error
+		//metadataAttr := devfile.Data.GetMetadata().Attributes
+		//dockerfilePath := metadataAttr.GetString("alpha.build-dockerfile", &err)
+		//if err != nil {
+		//	fmt.Printf("err: %v\n", err)
+		//}
+		//fmt.Printf("dockerfilePath: %s\n", dockerfilePath)
 	}
 
 }
