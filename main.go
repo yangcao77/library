a77b6af2675c6f3352227fb24b751375de32d0e5
Stephanie <yangcao@redhat.com>
2024-02-15 10:29:43 -0500

    generate script
    Signed-off-by: Stephanie <yangcao@redhat.com>

--- main.go
+++ main.go
@@ -18,13 +18,10 @@ package main
 import (
 	"fmt"
 	"os"
-	"reflect"
 	"strings"
 
 	devfilepkg "github.com/devfile/library/v2/pkg/devfile"
 	"github.com/devfile/library/v2/pkg/devfile/parser"
-	v2 "github.com/devfile/library/v2/pkg/devfile/parser/data/v2"
-	"github.com/devfile/library/v2/pkg/devfile/parser/data/v2/common"
 )
 
 func main() {
@@ -55,87 +52,13 @@ func parserTest() {
 		}
 		fmt.Println("parsing devfile from ./devfile.yaml")
 	}
-	devfile, warning, err := devfilepkg.ParseDevfileAndValidate(args)
+	_, warning, err := devfilepkg.ParseDevfileAndValidate(args)
 	if err != nil {
 		fmt.Println(err)
 	} else {
 		if len(warning.Commands) > 0 || len(warning.Components) > 0 || len(warning.Projects) > 0 || len(warning.StarterProjects) > 0 {
 			fmt.Printf("top-level variables were not substituted successfully %+v\n", warning)
 		}
-		devdata := devfile.Data
-		if (reflect.TypeOf(devdata) == reflect.TypeOf(&v2.DevfileV2{})) {
-			d := devdata.(*v2.DevfileV2)
-			fmt.Printf("schema version: %s\n", d.SchemaVersion)
-		}
-
-		components, e := devfile.Data.GetComponents(common.DevfileOptions{})
-		if e != nil {
-			fmt.Printf("err: %v\n", err)
-		}
-		fmt.Printf("All component: \n")
-		for _, component := range components {
-			fmt.Printf("%s\n", component.Name)
-		}
-
-		fmt.Printf("All Exec commands: \n")
-		commands, e := devfile.Data.GetCommands(common.DevfileOptions{})
-		if e != nil {
-			fmt.Printf("err: %v\n", err)
-		}
-		for _, command := range commands {
-			if command.Exec != nil {
-				fmt.Printf("command %s is with kind: %s\n", command.Id, command.Exec.Group.Kind)
-				fmt.Printf("workingDir is: %s\n", command.Exec.WorkingDir)
-			}
-		}
-
-		fmt.Println("=========================================================")
-
-		compOptions := common.DevfileOptions{
-			Filter: map[string]interface{}{
-				"tool": "console-import",
-				"import": map[string]interface{}{
-					"strategy": "Dockerfile",
-				},
-			},
-		}
-
-		components, e = devfile.Data.GetComponents(compOptions)
-		if e != nil {
-			fmt.Printf("err: %v\n", err)
-		}
-		fmt.Printf("Container components applied filter: \n")
-		for _, component := range components {
-			if component.Container != nil {
-				fmt.Printf("%s\n", component.Name)
-			}
-		}
-
-		cmdOptions := common.DevfileOptions{
-			Filter: map[string]interface{}{
-				"tool": "odo",
-			},
-		}
-
-		fmt.Printf("Exec commands applied filter: \n")
-		commands, e = devfile.Data.GetCommands(cmdOptions)
-		if e != nil {
-			fmt.Printf("err: %v\n", err)
-		}
-		for _, command := range commands {
-			if command.Exec != nil {
-				fmt.Printf("command %s is with kind: %s", command.Id, command.Exec.Group.Kind)
-				fmt.Printf("workingDir is: %s\n", command.Exec.WorkingDir)
-			}
-		}
-
-		var err error
-		metadataAttr := devfile.Data.GetMetadata().Attributes
-		dockerfilePath := metadataAttr.GetString("alpha.build-dockerfile", &err)
-		if err != nil {
-			fmt.Printf("err: %v\n", err)
-		}
-		fmt.Printf("dockerfilePath: %s\n", dockerfilePath)
 	}
 
 }
